d1e167a3579665854df085489379080a1130ac2f
Aga <aga.k396@gmail.com>
2020-03-02 11:40:37 +0100

    Add indication od current image after clicking thumbnail

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,18 @@
 function activateGallery() {
-  let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
       // what will happen when thumbnail is clicked
       let newImageSrc = thumbnail.dataset.largeVersion;
+      let newTitle = thumbnail.dataset.title;
+
       mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newTitle);
+
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
     });
   });
 }
