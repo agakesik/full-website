bce68bb99b521a7f04e60a20940dc3ce9083e969
Aga <aga.k396@gmail.com>
2020-03-02 10:44:26 +0100

    Add function to change main photo when clicked on thumbnail

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,12 @@
 function activateGallery() {
-  alert("Hello from gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // what will happen when thumbnail is clicked
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+    });
+  });
 }
