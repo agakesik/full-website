971464795d7050cd508b4e4e0d29e1453815689a
Aga <aga.k396@gmail.com>
2020-03-02 12:02:05 +0100

    Finish setting up responsive gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,19 +1,31 @@
+// Activating image gallery
+// The main task is to attach event listener to each thumbnail photo
+// And the gallery to respond appropriatry
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
-      // what will happen when thumbnail is clicked
       let newImageSrc = thumbnail.dataset.largeVersion;
       let newTitle = thumbnail.dataset.title;
-      let currentClass = "current"
 
+      // Change main image
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", newTitle);
 
+      // Change which image is current
+      let currentClass = "current"
       document.querySelector("."+currentClass).classList.remove(currentClass);
       thumbnail.parentNode.classList.add(currentClass);
+
+      // Update main photo info
+      let galleryInfo = document.querySelector("gallery-info");
+      let title = document.querySelector(".title");
+      let description = document.querySelector(".description");
+
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
